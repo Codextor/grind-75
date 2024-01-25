def938d36b7bff6d4337f0834ad1947b1aabe757
Sandeep Pandey <fromsandeeppandey@gmail.com>
2024-01-26 03:00:31 +0530

    Add kotlin solution for invert binary tree

---
+++ grind-75-kotlin/src/main/kotlin/InvertBinaryTree.kt
@@ -0,0 +1,49 @@
+/**
+ * Given the root of a binary tree, invert the tree, and return its root.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [4,2,7,1,3,6,9]
+ * Output: [4,7,2,9,6,3,1]
+ * Example 2:
+ *
+ *
+ * Input: root = [2,1,3]
+ * Output: [2,3,1]
+ * Example 3:
+ *
+ * Input: root = []
+ * Output: []
+ *
+ *
+ * Constraints:
+ *
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ * @see <a href="https://leetcode.com/problems/invert-binary-tree/">LeetCode</a>
+ *
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+fun invertTree(root: TreeNode?): TreeNode? {
+    root?.let { root ->
+        val tempNode = root.left
+        root.left = root.right?.also { invertTree(it) }
+        root.right = tempNode?.also { invertTree(it) }
+    }
+    return root
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
