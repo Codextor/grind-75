0765871df2989db6ddf6d3e77d8891eb1583f8e0
Sandeep Pandey <fromsandeeppandey@gmail.com>
2024-01-31 20:08:26 +0530

    Make method private

--- grind-75-kotlin/src/main/kotlin/BinarySearch.kt
+++ grind-75-kotlin/src/main/kotlin/BinarySearch.kt
@@ -30,7 +30,7 @@ fun search(nums: IntArray, target: Int): Int {
     return binarySearch(nums, target, 0, nums.size - 1)
 }
 
-fun binarySearch(nums: IntArray, target: Int, start: Int, end: Int): Int {
+private fun binarySearch(nums: IntArray, target: Int, start: Int, end: Int): Int {
     if (start > end) {
         return -1
     }
