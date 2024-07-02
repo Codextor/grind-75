3dcaf3819d841b3d95dad09c775a36466b27e12f
Sandeep Pandey <fromsandeeppandey@gmail.com>
2024-07-03 05:26:19 +0530

    Add kotlin test for binary search

---
+++ grind-75-kotlin/src/test/kotlin/BinarySearchTest.kt
@@ -0,0 +1,19 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class BinarySearchTest {
+
+    @Test
+    fun testSearch() {
+        val testCases = listOf(
+            Pair(Pair(intArrayOf(-1, 0, 3, 5, 9, 12), 9), 4),
+            Pair(Pair(intArrayOf(-1, 0, 3, 5, 9, 12), 2), -1)
+        )
+
+        testCases.forEach { test ->
+            val (input, expected) = test
+            val (nums, target) = input
+            assertEquals(expected, search(nums, target))
+        }
+    }
+}
