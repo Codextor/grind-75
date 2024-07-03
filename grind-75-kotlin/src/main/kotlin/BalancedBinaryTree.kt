a83c3a1894b2ce24ef8196be8f6eeaae378b3e44
Sandeep Pandey <fromsandeeppandey@gmail.com>
2024-07-04 03:09:23 +0530

    Fix comment

--- grind-75-kotlin/src/main/kotlin/BalancedBinaryTree.kt
+++ grind-75-kotlin/src/main/kotlin/BalancedBinaryTree.kt
@@ -27,7 +27,7 @@ import kotlin.math.abs
  * Constraints:
  *
  * The number of nodes in the tree is in the range [0, 5000].
- * -104 <= Node.val <= 104
+ * -10^4 <= Node.val <= 10^4
  * @see <a href="https://leetcode.com/problems/balanced-binary-tree/">LeetCode</a>
  *
  * Example:
